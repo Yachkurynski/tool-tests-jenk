db1242cf53603b7f3608c3d67bb1c5730f6fb624
Aliaksei Breilian <aliaksei_breilian@epam.com>
2019-07-16 18:08:34 +0300

    add file ISuiteActions.java

---
+++ com/tool/automation/generated/base/ISuiteActions.java
@@ -0,0 +1,17 @@
+
+package com.tool.automation.generated.base;
+
+import java.util.List;
+
+public interface ISuiteActions
+    extends ICommonActions
+{
+
+
+    void hasTestsList(List<String> testsList);
+
+    void addTest(String testName);
+
+    void openTest(String testName);
+
+}
