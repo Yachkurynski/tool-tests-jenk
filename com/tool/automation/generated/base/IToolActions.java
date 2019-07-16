3ad8782ab2c93e3e4ae867a8149a96c9406e2f02
Aliaksei Breilian <aliaksei_breilian@epam.com>
2019-07-16 18:08:46 +0300

    add file IToolActions.java

---
+++ com/tool/automation/generated/base/IToolActions.java
@@ -0,0 +1,16 @@
+
+package com.tool.automation.generated.base;
+
+
+public interface IToolActions
+    extends ICommonActions
+{
+
+
+    void toCasesTab();
+
+    void toKeywordsTab();
+
+    void toTemplatesTab();
+
+}
