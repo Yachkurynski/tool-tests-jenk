00f4029ddd365bfcb79319bc3fc9ea8a1f6d6ff0
Pavel Yachkurynski <Pavel_Yachkurynski@epam.com>
2019-08-08 14:51:48 +0300

    remove dump to another dir

--- src/main/java/com/tool/automation/implementation/ui/UIToolActions.java
+++ src/main/java/com/tool/automation/implementation/ui/UIToolActions.java
@@ -3,7 +3,7 @@ package com.tool.automation.implementation.ui;
 import com.tool.automation.model.enums.ToolTabs;
 import com.tool.automation.generated.base.IToolActions;
 import com.tool.automation.model.ui.pages.KeywordsPage;
-import org.apache.commons.lang3.StringUtils;
+import org.apache.commons.io.FileUtils;
 
 public class UIToolActions extends UICommonActions implements IToolActions {
 
@@ -31,7 +31,7 @@ public class UIToolActions extends UICommonActions implements IToolActions {
     if (!keywordsPage.isImportButtonPresent()) {
       keywordsPage.clearData();
     }
-    fileName = StringUtils.substring(getClass().getClassLoader().getResource(fileName).getPath(), 1);
+    fileName = FileUtils.getFile("data-dump/" + fileName).getAbsolutePath();
     getFactory().getKeywordsPage().importData(fileName);
   }
 
